8bf6886acd8da49a134d173d584815bbd7eb2e37
Chiara-brtn <chiara.bertinotti@yahoo.com>
2025-05-09 16:33:37 +0200

    Ajout Boutons

--- src/main/java/twizzies/Button.java
+++ src/main/java/twizzies/Button.java
@@ -1,27 +1,55 @@
 package twizzies;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Button {
     String fichierImage;
 
-    public Button(String nom, JPanel pannel, String fichierImage, int x, int y, int width, int height) {
+    // le bouton prend en compte le frame pour le placement de l'image au centre
+    public Button(Frame f, String nom, JPanel pannel, String fichierImage, int x, int y, int width, int height) {
         this.fichierImage = fichierImage;
         JButton bouton = new JButton(nom);
+        bouton.setBackground(Color.pink);
         bouton.setBounds(x, y, width, height);
         pannel.add(bouton);
-
+        pannel.setBackground(new Color(230, 223, 204));
         bouton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
                 // inserer ici le traitement d'image
+                insererImage(f, fichierImage);
+
             }
         });
 
+    }
+
+    // fonction pour inserer l'image
+    public void insererImage(JFrame f, String fichierImage) {
+        try {
+            // création d'un Jpanel pour placer l'image
+            JPanel panel = new JPanel();
+            panel.setBackground(new Color(230, 223, 204));
+            panel.setBounds(70, 70, 100, 100);
+            BufferedImage img = ImageIO.read(new File(fichierImage));
+            JLabel pic = new JLabel(new ImageIcon(img));
+            panel.add(pic);
+            f.add(panel);
+            // recharger la frame pour faire apparaitre l'image
+            f.setVisible(true);
+        } catch (IOException e) {
+        }
     }
 
 }
